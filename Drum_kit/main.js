661acd2ccb0d769ea33440f0e61a322bee64d3e4
Osamuyi <muyixone@yahoo.com>
2023-04-04 17:36:24 +0100

    Try to implement mouse event

--- Drum_kit/main.js
+++ Drum_kit/main.js
@@ -1,3 +1,5 @@
+const keys = document.getElementById('keys');
+const mainContainer = document.getElementById('outer_container');
 function playAudio(event) {
   // Used event.key instead of event.keycode because the later
   // is deprecated in javascript
@@ -7,6 +9,7 @@ function playAudio(event) {
 
   if (!audio) return;
 
+  // Make audio repeatable when key is pressed multiple times
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
@@ -21,6 +24,24 @@ function removePlayingClass(event) {
   key.classList.remove('playing');
 }
 
+// window.addEventListener('click', (e) => {
+//   if (e.target === keys) {
+//     return console.log('heya');
+//   }
+
+//   if (e.target.tagName == 'KBD') {
+//     const audioKey = e.target.textContent;
+//   }
+// });
+
 window.addEventListener('keydown', playAudio);
 
 window.addEventListener('keyup', removePlayingClass);
+
+// document.addEventListener('click', (e) => {
+//   const key = document.querySelector(`audio[data-key="${e.target.innerHTML}"]`);
+
+//   const outsideClick = !elem.contains(e.target.innerHTML);
+
+//   console.log(outsideClick);
+// });
