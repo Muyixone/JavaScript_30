2a54e75e0720e38a93165791adfbc1b4b6c64c18
Osamuyi <muyixone@yahoo.com>
2023-04-06 11:21:41 +0100

    Make the spacing effect and the base color to function independent of the blur function

--- CSS _variables/main.js
+++ CSS _variables/main.js
@@ -3,15 +3,16 @@ const blur = document.getElementById('blur');
 const baseColor = document.getElementById('color_picker');
 const borderSpacing = document.getElementById('spacing');
 
-const targetImg = document.getElementById('wall_image');
-
 function blurItem(event) {
   rootDom.style.setProperty('--Blur', `blur(${event.target.value}px)`);
-  rootDom.style.setProperty('--base-color', `${event.target.value}`);
 }
 
 blur.addEventListener('input', blurItem);
-baseColor.addEventListener('input', blurItem);
+
+baseColor.addEventListener('input', (e) => {
+  rootDom.style.setProperty('--base-color', `${e.target.value}`);
+});
+
 borderSpacing.addEventListener('input', (e) => {
   rootDom.style.setProperty('--spacing', `${e.target.value}px`);
 });
